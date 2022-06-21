99c237f716b534f59b79ea589fd5dc473e759219
Luciana U <luciana.uruena@hotmail.com>
2022-06-21 20:27:11 -0300

    Conectando con Firebase

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,3 +12,19 @@ root.render(
     
   </React.StrictMode>
 );
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCh7vuCmwvuLURlpxqNjTgWx7ZfffrqMBE",
+  authDomain: "coderhouse-uruenareact.firebaseapp.com",
+  projectId: "coderhouse-uruenareact",
+  storageBucket: "coderhouse-uruenareact.appspot.com",
+  messagingSenderId: "857457630381",
+  appId: "1:857457630381:web:f7f73c92b929b852de3a24",
+  measurementId: "G-CWNK6R33LT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
---
+++ src/util/firebaseConfig.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCh7vuCmwvuLURlpxqNjTgWx7ZfffrqMBE",
+    authDomain: "coderhouse-uruenareact.firebaseapp.com",
+    projectId: "coderhouse-uruenareact",
+    storageBucket: "coderhouse-uruenareact.appspot.com",
+    messagingSenderId: "857457630381",
+    appId: "1:857457630381:web:f7f73c92b929b852de3a24",
+    measurementId: "G-CWNK6R33LT"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
+  const db = getFirestore(app)
+
+export default db
